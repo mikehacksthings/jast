cb02fa08b77af28f4d9b7ec7a09283d4ad0a5635
Mike Lisi <mike@mikehacksthings.com>
2019-02-13 22:52:53 -0500

    Updated report format.

--- jast.py
+++ jast.py
@@ -137,10 +137,14 @@ class Report:
 				<title>JAST Report</title>
 				<link rel="stylesheet" type="text/css" href="style.css">
 				<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 			</head>
-		<body>
+		<body class="m-4 p-4">
 		<center>
-		<h1> JAST Report </h1>
+		<nav class="navbar fixed-top navbar-dark bg-dark">
+		  <a class="navbar-brand brand-text mx-auto" href="#">JAST Report</a>
+		</nav>
 		"""
 
 		self.footer = """
@@ -182,16 +186,19 @@ class Report:
 		f.close()
 
 	def write_host(self, host):
+		host_data = "<div class =\"card mt-4 mb-4\">\n"
+
 		if host.error:
-			host_data = "<h2>Host: {0}</h2><br>\n<h5>Error taking screenshot:</h5><br>\n{1}<br>\n".format(host.get_url(), host.error_msg)
+			host_data += "\t<div class =\"card-footer\">\n\t\t<p class=\"card-text\"><h4>"
+			host_data += "{0}</h4>\n<h5>Error taking screenshot:</h5><br>\n{1}<br>\n".format(host.get_url(), host.error_msg)
 			f = open(self._report_dir + "/report.html", 'a')
 			f.write(host_data)
-			f.write("<hr><br>")
 			f.close()
 		else:
+			host_data += "\t<img src=\"{0}\" class =\"card-img-top\" />\n".format(host.get_ss_filename())
+			host_data += "\t<div class =\"card-footer\">\n\t\t<p class=\"card-text\"><h4>"
+			host_data += "<a href=\"{0}\" target=\"_blank\">{1}</a></h4>".format(host.get_url(), host.get_url())
 
-			host_data = "<h2>Host: <a href=\"{0}\" target=\"_blank\">{1}</a></h1>".format(host.get_url(), host.get_url())
-			host_data += "<img src=\"{0}\" /><br>".format(host.get_ss_filename())
 			f = open(self._report_dir + "/report.html", 'a')
 			f.write(host_data)
 
@@ -199,7 +206,7 @@ class Report:
 				for key, value in host.get_headers().items():
 					f.write("<b>" + key + ": " + value + "</b><br>")
 
-			f.write("<hr><br>")
+			f.write("</p>\n\t</div>\n</div>")
 		f.close()
 
 	def finish(self):
@@ -289,9 +296,13 @@ if __name__ == '__main__':
 
 	print("\033[0;32m[+]\033[0m Processing host(s)...")
 	if args['-f']:
-		f = open(args['-f'], 'r')
-		data = f.readlines()
-		f.close()
+		if os.path.exists(args['-f']) and os.path.isfile(args['-f']):
+			f = open(args['-f'], 'r')
+			data = f.readlines()
+			f.close()
+		else: # File doesn't exist
+			print("\033[0;31m[!]\033[0m Host file not found! Exiting.")
+			sys.exit(-1)
 
 	elif args['-u']:
 		data = [args['-u']]
@@ -310,6 +321,7 @@ if __name__ == '__main__':
 		take_screenshot(host, browser, args)
 	browser.close()
 
+	report.start()
 	for host in hosts:
 		report.write_host(host)
 	report.finish()
